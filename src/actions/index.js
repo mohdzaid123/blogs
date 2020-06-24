6ff0c46c620ac046483231974bb8002aa82e51fd
mohd-zaid <zaidmohd130399@gmail.com>
2020-06-25 01:51:34 +0530

    Add Reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,15 @@
+import _ from "lodash";
 import JsonPlaceholder from "../apis/JsonPlaceholder";
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await JsonPlaceholder.get("/posts");
 
-  dispatch({ type: "FETCH_POSTS", payload: response });
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
+
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await JsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: response.data });
+});
