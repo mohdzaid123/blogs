c06fd27c8c11ec5b2a6756b5c19f26f7a6340aea
mohd-zaid <zaidmohd130399@gmail.com>
2021-03-25 12:44:48 +0530

    Updates

---
+++ precache-manifest.ec73ae709892690178882cf3522b52fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49853138de87e57f93d44e8833d459bb",
+    "url": "/blogs/index.html"
+  },
+  {
+    "revision": "dc9f10a5ccb3351ba815",
+    "url": "/blogs/static/js/2.557b56b4.chunk.js"
+  },
+  {
+    "revision": "93e25e572c8d0aba40c780e420250f66",
+    "url": "/blogs/static/js/2.557b56b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "019eb892e3261e04a8d6",
+    "url": "/blogs/static/js/main.a8943e74.chunk.js"
+  },
+  {
+    "revision": "4e1b2a8e5495b372cce8",
+    "url": "/blogs/static/js/runtime-main.94b72b9b.js"
+  }
+]);
